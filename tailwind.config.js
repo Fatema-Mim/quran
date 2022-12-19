0ef292a01b60fb8c78f1a737e75037738746f992
Mim <fatemamim28@gmail.com>
2022-12-19 17:18:24 +0600

    add bgimg

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,10 @@ module.exports = {
       },
       backgroundImage: {
         'bg1': "url('/public/img/background/bg1.png')",
-        'bg2': "url('/public/img/background/bg2.png')",
+        'bg2': "url('/public/img/background/arabic1.png')",
+        'bg4': "url('/public/img/background/arabic2.png')",
         'bg3': "url('/public/img/background/bg3.png')",
+        'bgNumber': "url('/public/img/suraName/numberbg.svg')",
       },
       colors:{
         color1:'#001D1D',
@@ -29,7 +31,6 @@ module.exports = {
       fontSize: {
         text1: ['18px', '22px'],
         text2: ['50px', '50.15px'],
-        text12: ['40px', '34.75px'],
         text3: ['24px', '34.75px'],
         text4: ['22px', '34.17px'],
         text5: ['16px', '23.17px'],
@@ -39,6 +40,9 @@ module.exports = {
         text9: ['10px', '12.1px'],
         text10: ['16px', '19.36px'],
         text11: ['19px', '22 px'],
+        text12: ['40px', '34.75px'],
+        text13: ['14px', '16.94px'],
+        text14: ['20px', '30px'],
       },
       boxShadow: {
         'shadowbtn': '0 4px 4px rgba(0, 0, 0, 0.7)',
